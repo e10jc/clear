134104f99636917f83205f836530f21e4924e387
Ethan Jon <e10jc@users.noreply.github.com>
2018-08-06 05:47:16 -0700

    building for production

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
 import NextApp, {Container} from 'next/app'
-import React from 'react'
+import * as React from 'react'
 
-export const SessionDataContext = React.createContext({})
+export const SessionDataContext = React.createContext({
+  facebookUser: null,
+  twitterUser: null,
+})
 
-class App extends NextApp {
+interface Props {
+  componentProps: any,
+  sessionDataProps: any,
+}
+
+class App extends NextApp<Props> {
+  state = {
+    sessionDataProps: this.props.sessionDataProps,
+  }
+
   static async getInitialProps ({Component, ctx}) {
     return {
       componentProps: Component.getInitialProps ? await Component.getInitialProps(ctx) : {},
@@ -11,13 +23,6 @@ class App extends NextApp {
         facebookUser: ctx.req ? ctx.req.session.facebookUser : {},
         twitterUser: ctx.req ? ctx.req.session.twitterUser : {},
       },
-    }
-  }
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      sessionDataProps: props.sessionDataProps,
     }
   }
 
